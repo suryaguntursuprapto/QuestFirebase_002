64000468f0394b5011dae54206ab2b023bb327ef
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-12 14:04:12 +0700

    Membuat object DestinasiHome pada DestinasiNavigasi

--- app/src/main/java/com/example/rdbms_20220140002/Navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/rdbms_20220140002/Navigation/DestinasiNavigasi.kt
@@ -4,3 +4,8 @@ interface DestinasiNavigasi{
     val route: String
     val titleRes: String
 }
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
