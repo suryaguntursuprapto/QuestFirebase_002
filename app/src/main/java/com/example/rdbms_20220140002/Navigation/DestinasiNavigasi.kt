9ede01d314deb3edf21d195d8f4171f20ed000d8
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-12 14:04:59 +0700

    Membuat object DestinasiInsert pada DestinasiNavigasi

--- app/src/main/java/com/example/rdbms_20220140002/Navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/rdbms_20220140002/Navigation/DestinasiNavigasi.kt
@@ -9,3 +9,8 @@ object DestinasiHome: DestinasiNavigasi{
     override val route: String = "home"
     override val titleRes: String = "Home"
 }
+
+object DestinasiInsert: DestinasiNavigasi{
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
