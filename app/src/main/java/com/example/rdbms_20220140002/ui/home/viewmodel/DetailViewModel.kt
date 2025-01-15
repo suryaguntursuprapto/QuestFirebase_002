24ff46e88863fa35f048631bc5b2e2420a07aba2
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-15 16:15:16 +0700

    Membuat DetailViewModel

--- app/src/main/java/com/example/rdbms_20220140002/ui/home/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/rdbms_20220140002/ui/home/viewmodel/DetailViewModel.kt
@@ -1,2 +1,41 @@
 package com.example.rdbms_20220140002.ui.home.viewmodel
 
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rdbms_20220140002.Repository.RepositoryMhs
+import com.example.rdbms_20220140002.model.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class DetailUiState {
+    data class Success(val mahasiswa: Flow<Mahasiswa>) : DetailUiState()
+    object Error : DetailUiState()
+    object Loading : DetailUiState()
+}
+
+class DetailViewModel(private val mhsRepository: RepositoryMhs) : ViewModel() {
+    var mhsDetailUiState: DetailUiState by mutableStateOf(DetailUiState.Loading)
+        private set
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getMhsByNim(nim: String) {
+        viewModelScope.launch {
+            mhsDetailUiState = DetailUiState.Loading
+            mhsDetailUiState = try {
+                val mahasiswa = mhsRepository.getMhs(nim)
+                DetailUiState.Success(mahasiswa)
+            } catch (e: IOException) {
+                DetailUiState.Error
+            } catch (e: HttpException) {
+                DetailUiState.Error
+            }
+        }
+    }
+}
