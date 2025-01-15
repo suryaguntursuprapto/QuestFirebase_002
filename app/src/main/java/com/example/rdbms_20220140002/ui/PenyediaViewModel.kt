6751c38e459fa0d97cf3eca5ed89d3efe528328f
suryaguntursuprapto <suryaguntur2000@gmail.com>
2025-01-15 16:16:11 +0700

    menambahkan DetailViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/rdbms_20220140002/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/rdbms_20220140002/ui/PenyediaViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.rdbms_20220140002.MahasiswaApplications
+import com.example.rdbms_20220140002.ui.home.viewmodel.DetailViewModel
 import com.example.rdbms_20220140002.ui.home.viewmodel.HomeViewModel
 import com.example.rdbms_20220140002.ui.home.viewmodel.InsertViewModel
 
@@ -18,6 +19,11 @@ object PenyediaViewModel{
         }
         initializer {
             InsertViewModel(
+                mahasiswaApp().container.repositoryMhs
+            )
+        }
+        initializer {
+            DetailViewModel(
                 mahasiswaApp().container.repositoryMhs
             )
         }
